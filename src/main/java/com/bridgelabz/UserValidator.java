08df83207ccb8e52381d97853bb01a95d71ab542
unknown <suyashgaikwad31@gmail.com>
2021-03-19 20:59:08 +0530

    added

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -5,8 +5,13 @@ import java.util.regex.Pattern;
 public class UserValidator {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
     public static boolean validateFirstName(String fname) {
         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fname).matches();
+    }
+    public static boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        return pattern.matcher(lname).matches();
     }
 }
--- src/test/java/com/bridgelabz/UserValidatorTest.java
+++ src/test/java/com/bridgelabz/UserValidatorTest.java
@@ -17,5 +17,17 @@ import org.junit.jupiter.api.Test;
         boolean result = UserValidator.validateFirstName("Su");
         Assertions.assertEquals(false,result);
     }
+    @Test
+     public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateLastName("Gaikwad");
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+     public void givenLastName_WhenShort_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = UserValidator.validateLastName("Ga");
+        Assertions.assertEquals(false,result);
+    }
 }
 
